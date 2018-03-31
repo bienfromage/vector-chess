5e879cbdddfe7aafaef7ae1b8d6e092823973613
bienfromage <100codemaster@gmail.com>
2018-03-30 21:28:45 -0400

    Added undo button

--- game.js
+++ game.js
@@ -1,18 +1,27 @@
 //canvas variables
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+
+
+var prevBlack = [{name:"rook",piece:"\u265C",x:0,y:0},{name:"rook",piece:"\u265C",x:7,y:0},{name:"knight",piece:"\u265E",x:1,y:0},{name:"knight",piece:"\u265E",x:6,y:0},{name:"bishop",piece:"\u265D",x:2,y:0},{name:"bishop",piece:"\u265D",x:5,y:0},{name:"queen",piece:"\u265B",x:3,y:0},{name:"king",piece:"\u265A",x:4,y:0}];
+var prevWhite = [{name:"rook",piece:"\u2656",x:0,y:7},{name:"rook",piece:"\u2656",x:7,y:7},{name:"knight",piece:"\u2658",x:1,y:7},{name:"knight",piece:"\u2658",x:6,y:7},{name:"bishop",piece:"\u2657",x:2,y:7},{name:"bishop",piece:"\u2657",x:5,y:7},{name:"queen",piece:"\u2655",x:3,y:7},{name:"king",piece:"\u2654",x:4,y:7}];
+
 var black = [{name:"rook",piece:"\u265C",x:0,y:0},{name:"rook",piece:"\u265C",x:7,y:0},{name:"knight",piece:"\u265E",x:1,y:0},{name:"knight",piece:"\u265E",x:6,y:0},{name:"bishop",piece:"\u265D",x:2,y:0},{name:"bishop",piece:"\u265D",x:5,y:0},{name:"queen",piece:"\u265B",x:3,y:0},{name:"king",piece:"\u265A",x:4,y:0}];
 for(i = 0; i < 8; i++){
   black.push({name:"pawn",piece:"\u265F",x:i,y:1});
+  prevBlack.push({name:"pawn",piece:"\u265F",x:i,y:1});
 }
 var white = [{name:"rook",piece:"\u2656",x:0,y:7},{name:"rook",piece:"\u2656",x:7,y:7},{name:"knight",piece:"\u2658",x:1,y:7},{name:"knight",piece:"\u2658",x:6,y:7},{name:"bishop",piece:"\u2657",x:2,y:7},{name:"bishop",piece:"\u2657",x:5,y:7},{name:"queen",piece:"\u2655",x:3,y:7},{name:"king",piece:"\u2654",x:4,y:7}];
 for(i = 0; i < 8; i++){
   white.push({name:"pawn",piece:"\u2659",x:i,y:6});
+  prevWhite.push({name:"pawn",piece:"\u2659",x:i,y:6});
 }
+
 var interval;
 var mouseItem;
 var mouseX;
 var mouseY;
+var isWhite = true;
 var player = white;
 var opponent = black;
 
@@ -92,6 +101,37 @@ function flipBoard(){
   var temp = player;
   player = opponent;
   opponent = temp;
+  
+  prevBlack = [];
+  for(i=0;i<black.length;i++)
+    prevBlack.push({name:black[i].name,piece:black[i].piece,x:black[i].x,y:black[i].y});
+    
+  prevWhite = [];
+  for(i=0;i<white.length;i++)
+    prevWhite.push({name:white[i].name,piece:white[i].piece,x:white[i].x,y:white[i].y});
+  
+  if(isWhite)
+    isWhite = false;
+  else
+    isWhite = true;
+}
+
+function reset(){
+  black = [];
+  for(i=0;i<prevBlack.length;i++)
+    black.push({name:prevBlack[i].name,piece:prevBlack[i].piece,x:prevBlack[i].x,y:prevBlack[i].y});
+    
+  white = [];
+  for(i=0;i<prevWhite.length;i++)
+    white.push({name:prevWhite[i].name,piece:prevWhite[i].piece,x:prevWhite[i].x,y:prevWhite[i].y});
+    
+  if(isWhite){
+    player = white;
+    opponent = black;
+  }else{
+    player = black;
+    opponent = white;
+  }
 }
 
 function listener(event, playerArray, opponentArray){
@@ -111,17 +151,25 @@ function listener(event, playerArray, opponentArray){
   var pieceX = Math.floor((x-interval/2*1.1)/interval);
   var pieceY = Math.ceil((y-interval*0.9)/interval);
   if(mouseItem){
+    var canPlace = true;
     for(i = 0; i<opponentArray.length; i++){
       if(opponentArray[i].x === pieceX && opponentArray[i].y === pieceY){
         opponentArray.splice(i,1);
       }
     }
     
-    mouseItem.x=pieceX;
-    mouseItem.y=pieceY;
-    playerArray.push(mouseItem);
-    mouseItem = null;
-    canvas.style.cursor="-webkit-grab" || "grab";
+    for(i = 0; i <playerArray.length; i++){
+      if(playerArray[i].x === pieceX && playerArray[i].y === pieceY){
+        canPlace = false;
+      }
+    }
+    if(canPlace){
+      mouseItem.x=pieceX;
+      mouseItem.y=pieceY;
+      playerArray.push(mouseItem);
+      mouseItem = null;
+      canvas.style.cursor="-webkit-grab" || "grab";
+    }
   }else{
     for(i = 0; i <playerArray.length; i++){
       if(playerArray[i].x === pieceX && playerArray[i].y === pieceY){
